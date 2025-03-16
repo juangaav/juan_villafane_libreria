95433f56b27c673c2bbb96436ed472a9c4ac2502
juangaav <juangaav@gmail.com>
2025-03-15 21:25:57 -0500

    Update staticAnalysis.groovy

--- vars/staticAnalysis.groovy
+++ vars/staticAnalysis.groovy
@@ -1,30 +1,28 @@
 def call(boolean qualityGateFail = false, boolean abortPipeline = false) {
-    pipeline {
-        agent any
-        stages {
-            stage('Code Quality Analysis') {
-                steps {
-                    script {
-                        try {
-                            timeout(time: 5, unit: 'MINUTES') {
-                                sh 'echo "Ejecución de las pruebas de calidad de código"'
-                            }
-                        } catch (err) {
-                            echo 'Timeout reached while waiting for the code quality analysis to complete.'
-                            if (abortPipeline) {
-                                error 'Pipeline aborted due to timeout.'
-                            }
+    agent any
+    stages {
+        stage('Code Quality Analysis') {
+            steps {
+                script {
+                    try {
+                        timeout(time: 5, unit: 'MINUTES') {
+                            sh 'echo "Ejecución de las pruebas de calidad de código"'
                         }
+                    } catch (err) {
+                        echo 'Timeout reached while waiting for the code quality analysis to complete.'
+                        if (abortPipeline) {
+                            error 'Pipeline aborted due to timeout.'
+                        }
+                    }
 
-                        // Simulación de evaluación del QualityGate
-                        if (qualityGateFail) {
-                            echo 'QualityGate failed.'
-                            if (abortPipeline) {
-                                error 'Pipeline aborted due to QualityGate failure.'
-                            }
-                        } else {
-                            echo 'QualityGate passed.'
+                    // Simulación de evaluación del QualityGate
+                    if (qualityGateFail) {
+                        echo 'QualityGate failed.'
+                        if (abortPipeline) {
+                            error 'Pipeline aborted due to QualityGate failure.'
                         }
+                    } else {
+                        echo 'QualityGate passed.'
                     }
                 }
             }
