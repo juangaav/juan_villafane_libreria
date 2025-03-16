6ab31791582a431c977aa890f7687832fc2ec00f
juangaav <juangaav@gmail.com>
2025-03-15 22:53:21 -0500

    Update staticAnalysis.groovy actividad 4

--- vars/staticAnalysis.groovy
+++ vars/staticAnalysis.groovy
@@ -1,22 +1,38 @@
 def call(boolean qualityGateFail = false, boolean abortPipeline = false) {
+    echo "Ejecutando staticAnalysis.groovy"
+    
+    // Obtener el nombre de la rama actual desde el entorno de Jenkins
+    def branchName = env.BRANCH_NAME
+    
+    // Mostrar el nombre de la rama para depuración
+    echo "Nombre de la rama: ${branchName}"
+    
     try {
         timeout(time: 5, unit: 'MINUTES') {
             sh 'echo "Ejecución de las pruebas de calidad de código"'
         }
     } catch (err) {
-        echo 'Timeout reached while waiting for the code quality analysis to complete.'
+        echo 'Timeout mientras se esperaba el analisis de código.'
         if (abortPipeline) {
-            error 'Pipeline aborted due to timeout.'
+            error 'Pipeline terminado por timeout.'
         }
     }
 
     // Simulación de evaluación del QualityGate
     if (qualityGateFail) {
-        echo 'QualityGate failed.'
-        if (abortPipeline) {
-            error 'Pipeline aborted due to QualityGate failure.'
-        }
+        echo 'Fallo de QualityGate.'
+        abortPipelineIfRequired(branchName, abortPipeline)
     } else {
-        echo 'QualityGate passed.'
+        echo 'QualityGate exitoso.'
+    }
+}
+
+def abortPipelineIfRequired(String branchName, boolean abortPipeline) {
+    if (abortPipeline) {
+        error 'Pipeline terminado por fallo en QualityGate'
+    } else {
+        if (branchName == 'master' || branchName.startsWith('hotfix')) {
+            error 'Pipeline terminado por nombre de branch invalido (master o hotfix).'
+        }
     }
 }
